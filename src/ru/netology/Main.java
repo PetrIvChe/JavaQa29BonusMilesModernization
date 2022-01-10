10c02361655a58e71755c0f22f328ef4da090b44
PetrIvChe <cherniavskiipetr@gmail.com>
2022-01-10 09:29:00 +0300

    With price = 8630

--- src/ru/netology/Main.java
+++ src/ru/netology/Main.java
@@ -3,7 +3,7 @@ package ru.netology;
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 10_000;
+        int price = 8630;
         int miles = service.calculate(price);
         System.out.println(miles);
     }
